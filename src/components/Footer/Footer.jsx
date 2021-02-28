3299428a9bc937c03f90d32760e8c0ab01512759
dayang4321 <dayang4321@users.noreply.github.com>
2021-02-28 16:43:16 +0100

    implement header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,17 +4,17 @@ import { ReactComponent as Phone } from '../../assets/img/svg/phone.svg'
 
 import FooterLogo from '../../assets/img/svg/groupbuy-shop.svg'
 import './Footer.css'
+import { Col, Row } from 'react-bootstrap';
 
 function Footer() {
 
     return(
         <footer>
-            <div className="mt-3 d-flex flex-column align-items-center mb-3">
-                <a className="contact-link" href="mailto:hello@declutter.ng"><i><Mail className="mr-3"/></i>hello@painless.ng</a>
-                <a className="contact-link mb-3" href="tel:+2348136526089"><i><Phone className="mr-3" /></i>08136526089</a>
-                <a  href className="contact-link d-block mt-4 buyer-link">Buy products on <strong>groupbuy.ng</strong></a>
-                
-            </div>
+            <Row className="mt-3 d-flex flex-column flex-md-row align-items-center mb-4 mb-md-5 justify-content-center">
+                <Col lg={{span:3, order: 0}} className="text-xl-right my-4 my-md-3"><a  className="contact-link" href="mailto:hello@declutter.ng"><i><Mail className="mr-3"/></i>hello@painless.ng</a></Col>
+                <Col lg={{span:3, order: 2}} className="text-xl-left my-4 my-md-3"><a  className="contact-link mb-3" href="tel:+2348136526089"><i><Phone className="mr-3" /></i>08136526089</a></Col>                
+                <Col lg={{span:4, order: 1}} className="text-xl-center my-5  my-md-4"><a  href className="contact-link mt-4 buyer-link">Buy products on <strong>groupbuy.ng</strong></a>    </Col>             
+            </Row>
             <img src={FooterLogo} alt="Gropubuy" />
             <div className="d-flex flex-column text-center painless-text mb-5">
                 <span>by</span>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,9 @@ import './Header.css'
 
 function Header() {
     return (
-        <header className="py-4">
-            <img className="py-2" src={Logo} alt="Groupbuy Logo"/>
+        <header className="py-4 position-relative">
+            <img className="py-2" src={Logo} alt="Groupbuy Logo" />
+            <a  href className="contact-link d-none d-lg-inline-block position-absolute m-0 buyer-link">Buy products on <strong>groupbuy.ng</strong></a>   
         </header>
     )
 }
